48c18da761be52d8d1f1f1b39fc2c8a49a17b7fe
Abarna <abarnamouli06@gmail.com>
2024-01-14 11:27:52 +0530

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'checkout', component: CheckoutComponent},
+  { path: 'details', component: DetailsComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent},
+  { path: '',   redirectTo: '/login', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component: NotfoundComponent }
+];

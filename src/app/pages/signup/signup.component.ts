451d79ce512a875d9c42c5fc4fde7a9be9c31ece
Abarna <abarnamouli06@gmail.com>
2024-01-28 14:57:53 +0530

    Test5

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -5,46 +5,31 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from "@angular/forms";
 import { FormGroup, FormControl } from "@angular/forms";
 import { Validators } from "@angular/forms";
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [MatButtonModule,CommonModule,FormsModule,ReactiveFormsModule],
+  imports: [MatButtonModule,CommonModule,FormsModule,ReactiveFormsModule,MatInputModule,MatFormFieldModule,MatCardModule,MatIconModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
   signUpForm = new FormGroup({
-    name: new FormControl("", [Validators.required, Validators.minLength(5)]),
-    birthDate: new FormGroup({
-      day: new FormControl("", [
-        Validators.required,
-        Validators.min(1),
-        Validators.max(31),
-      ]),
-      month: new FormControl("", [
-        Validators.required,
-        Validators.min(1),
-        Validators.max(12),
-      ]),
-      year: new FormControl("", [
-        Validators.required,
-        Validators.min(1900),
-        Validators.max(new Date().getFullYear()),
-      ]),
-    }),
-  });
+    Firstname: new FormControl("", [Validators.required, Validators.minLength(5)]),
+  })
+  hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
 
-  updateProfile() {
-    this.signUpForm.patchValue({
-      name: "Jane",
-      birthDate: {
-        day: "1",
-        month: "1",
-        year: "2000",
-      },
-    });
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
   }
-
   onSubmit() {
     console.log(this.signUpForm.value);
   }

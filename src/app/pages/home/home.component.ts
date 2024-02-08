57b280285d1fded85718ea6297d5ff8d8cdd4cf4
Abarna <abarnamouli06@gmail.com>
2024-02-08 22:57:07 +0530

    Test7

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,15 +4,22 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import{RouterOutlet}from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatSlideToggleModule,MatToolbarModule, MatIconModule,MatMenuModule,RouterOutlet],
+  imports: [CommonModule,FormsModule,MatInputModule,MatFormFieldModule,ReactiveFormsModule,MatButtonModule,MatSlideToggleModule,MatToolbarModule, MatIconModule,MatMenuModule,RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 
 export class HomeComponent {
-
+  value = "Search here!";
 }
